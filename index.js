09b3946ef56cd742ae1657691f7c8e8c9202d2c7
xena.kontopirakis <sydneykontopirakis@gmail.com>
2022-10-01 12:19:05 -0400

    commit tree a814dc8456e65a70d335e2b4ae9c20c6f623037a

---
+++ index.js
@@ -0,0 +1,2 @@
+function logTwoValues("xena").toEqual("Hi, my name is xena.") {console.log(`The two values are ${Name} and ${introduction}.`);}
+expect(introduction("xena")).toEqual("Hi, my name is xena.");
